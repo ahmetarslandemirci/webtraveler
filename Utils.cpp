8fed77f0d2c51b26cd15815c823595fae5119fab
ahmet <arslan.ahmet.demirci@gmail.com>
2018-12-22 20:22:38 +0300

    updated for 3xx status codes

--- Utils.cpp
+++ Utils.cpp
@@ -39,13 +39,15 @@ std::vector<std::string> Utils::findAll(const std::string &regex, const std::str
     return foundedVector;
 }
 
-std::string Utils::request(const std::string &url) {
+std::string Utils::request(std::string &url) {
     Poco::URI uri(url);
     std::string path(uri.getPathAndQuery());
     if (path.empty()) path = "/";
     //std::cout <<"Host:" << uri.getHost() << std::endl;
     Poco::Net::HTTPResponse response;
     Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_GET, path, Poco::Net::HTTPMessage::HTTP_1_1);
+    request.add("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0");
+    //request.add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
     std::ostringstream ss;
 
     if(uri.getScheme() == "http") {
@@ -61,7 +63,7 @@ std::string Utils::request(const std::string &url) {
         ss << s.rdbuf();
     }
     else if(uri.getScheme() == "https") {
-        Poco::Net::Context::Ptr pContext = new Poco::Net::Context(Poco::Net::Context::CLIENT_USE, "", "", "", Poco::Net::Context::VERIFY_NONE, 9, true, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
+        Poco::Net::Context::Ptr pContext = new Poco::Net::Context(Poco::Net::Context::CLIENT_USE, "", "", "", Poco::Net::Context::VERIFY_NONE);
         Poco::Net::HTTPSClientSession session(uri.getHost(), uri.getPort(), pContext );
         try {
             session.sendRequest(request);
@@ -72,17 +74,15 @@ std::string Utils::request(const std::string &url) {
         }
         std::istream& s = session.receiveResponse(response);
         ss << s.rdbuf();
+        //std::cout << "Here..."<< std::endl;
     }
 
-    //std::cout << response.get("Location") << std::endl;
-    if(response.getStatus() == 302) {
-        uri = Poco::URI(response.get("Location"));
-        return Utils::request(uri.toString());
+    if(response.getStatus() > 300 && response.getStatus() < 310) {
+        std::cout << "Loc:" << response.get("Location") << std::endl;
+        url = response.get("Location");
+        return Utils::request(url);
     }
-
-    if( response.getStatus() != 200)
-        return std::string();
-
+    //std::cout << "hOLLAA"<< std::endl;
     return ss.str();
 }
 
@@ -117,7 +117,7 @@ std::vector<std::string> Utils::fixUrls(const std::vector<std::string> &urlList,
     return uniqUrls;
 }
 
-void Utils::searchInPages(const std::string &url, std::vector<std::string> &nextList, const std::string &regex, std::vector<std::string> &data, unsigned long limit) {
+void Utils::searchInPages(std::string &url, std::vector<std::string> &nextList, const std::string &regex, std::vector<std::string> &data, unsigned long limit) {
     std::string source = Utils::request(url);
     std::vector<std::string> founded = Utils::findAll(regex, source);
     std::vector<std::string>::iterator it;
@@ -151,23 +151,22 @@ void Utils::searchInPages(const std::string &url, std::vector<std::string> &next
     }
 }
 
-void Utils::searchInPages(const std::string &url, const std::string &regex, std::vector<std::string> &data, unsigned long limit) {
+void Utils::searchInPages(std::string &url, const std::string &regex, std::vector<std::string> &data, unsigned long limit) {
     std::deque<std::string> queue,visited;
     queue.push_back(url);
 
     std::deque<std::string>::iterator it_que,it_vis;
     std::vector<std::string>::iterator it;
 
-    unsigned long visitedPageCount = 0;
-    while(!queue.empty() && visitedPageCount < limit) {
+    while(!queue.empty() && visited.size() < limit) {
         std::string url = queue.front();
         queue.pop_front();
 
-        std::cout << "URL => " << url << std::endl;
+        //std::cout << "URL => " << url << std::endl;
         std::string source = Utils::request(url);
 
         // Search for regex data
-        //std::cout << source << std::endl;
+        //std::cout << "s:" << source << std::endl;
         std::vector<std::string> founded = Utils::findAll(regex, source);
         std::cout << queue.size() << " : " << visited.size() << std::endl;
         for(unsigned long i = 0;i<founded.size();++i) {
@@ -182,7 +181,7 @@ void Utils::searchInPages(const std::string &url, const std::string &regex, std:
         std::vector<std::string> links = Utils::fixUrls(Utils::findAll(hrefRegex, source),url);
         visited.push_back(url);
         //std::cout << "Lnk size: " << links.size() << std::endl;
-        for(int i = 0; i<links.size() ;i++) {
+        for(int i = 0; i < links.size() ;i++) {
 
             it_que = std::find(queue.begin(), queue.end(), links.at(i));
             it_vis = std::find(visited.begin(), visited.end(), links.at(i));
@@ -190,8 +189,6 @@ void Utils::searchInPages(const std::string &url, const std::string &regex, std:
                 queue.push_back(links.at(i));
             }
         }
-
-        visitedPageCount+=1;
     }
 }
 
