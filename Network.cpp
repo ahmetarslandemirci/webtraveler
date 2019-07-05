ec2b9c5d67772ff57b2c62990e760f44102bea22
arslan <dont-send-this-arslan@yopmail.com>
2019-07-05 03:18:55 +0300

    updated network functions with libcurl

--- Network.cpp
+++ Network.cpp
@@ -1,38 +1,27 @@
 #include "Network.h"
-#include <netdb.h>
+#include <curl/curl.h>
 
-#include <iostream> // for debug
-
-bool Network::checkDNS(const std::string &url) {
-    hostent *host = NULL;
-    host = gethostbyname(url.c_str());
-    if(host != NULL) {
-        return true;
-    }
-    return false;
+uint Network::write_cb(char *in, uint size, uint nmemb, std::string *out) {
+    out->append(in);
+    return size * nmemb;
 }
 
-
 std::string Network::request(const std::string &url) {
-    CurlWrapper *curl = CurlWrapper::create(true);
-    curl->setOption(CURLOPT_URL, url.c_str());
-    curl->setOption(CURLOPT_HEADER, 0L);
-    curl->setOption(CURLOPT_FOLLOWLOCATION, 1L);
-    curl->setOption(CURLOPT_TIMEOUT, 20L);
-    curl->setOption(CURLOPT_SSL_VERIFYHOST, 0L);
-    curl->setOption(CURLOPT_SSL_VERIFYPEER, 0L);
-    curl->setOption(CURLOPT_MAXREDIRS, 5);
-    using namespace std;
-    string userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.1 Safari/605.1.15";
-    map<string,string> headers;
-    headers.insert(pair<string,string>("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"));
-    headers.insert(pair<string,string>("Accept-Language","en-US,en;q=0.5"));
-    headers.insert(pair<string,string>("User-Agent",userAgent));
+    CURL *curl;
+    std::string source, user_agent;
 
-    curl->setHeaders(headers);
-    string response = curl->exec();
-    delete curl;
+    curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15";
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, user_agent.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &source);
+    curl_easy_perform(curl);
 
-    return response;
+    curl_easy_cleanup(curl);
+    return source;
 }
 
--- Network.h
+++ Network.h
@@ -1,23 +1,17 @@
 #ifndef WEBTRAVELER_NETWORK_H
 #define WEBTRAVELER_NETWORK_H
 #include <string>
-#include <curlwrapper.h>
 
 class Network {
 public:
-    /**
-     * This function will check the DOMAIN name is existing.
-     * @param url domain name without protocol (like google.com)
-     * @return if domain name exist it'll return true else return false
-     */
-    static bool checkDNS(const std::string &url);
-
     /**
      * This function will make a GET request and return response to you.
      * @param url an url address
      * @return response of server
      */
     static std::string request(const std::string &url);
+
+    static uint write_cb(char *in, uint size, uint nmemb, std::string *out);
 };
 
 
